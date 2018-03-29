e032723b9359538dd5de1f9faf89c742d38cc3c3
a5710122 <a5701012610122@email.kmutnb.ac.th>
2018-03-29 14:15:05 +0700

    Creat poll but error template

--- test29/settings.py
+++ test29/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'test29.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['quiz/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- test29/urls.py
+++ test29/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from quiz import views
+
+app_name = 'quiz'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
