fbddbd3b0574f7a0dbc4c8777050a19fa1b8de8c
Narudon <s5701012610122@email.kmutnb.ac.th>
2018-06-11 22:38:00 +0700

    Write views more

--- quiz/views.py
+++ quiz/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.template import loader
+
+from .models import Question
 
 def IndexView(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('quiz/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 
