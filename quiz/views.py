6af68c6eee3787faf356034139831f72ffd2997b
Narudon <s5701012610122@email.kmutnb.ac.th>
2018-06-11 23:39:54 +0700

    Use generic views Less code

--- quiz/views.py
+++ quiz/views.py
@@ -6,18 +6,23 @@ from django.views import generic
 
 from .models import Question, Choice
 
-def IndexView(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'quiz/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'quiz/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'quiz/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'quiz/results.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'quiz/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'quiz/results.html'
 
 
 def vote(request, question_id):
