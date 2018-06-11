0d275666d3bdf37f440f4d3a3cac28bb0b1e5c9d
Narudon <s5701012610122@email.kmutnb.ac.th>
2018-06-11 23:35:37 +0700

    Write views more for see results vote

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'quiz'
 urlpatterns = [
     path('', views.IndexView, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
