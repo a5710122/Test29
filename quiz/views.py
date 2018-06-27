2e9d9d9c4e4ccf51bc21d1bf8880abc75e59f1f5
Narudon <s5701012610122@email.kmutnb.ac.th>
2018-06-28 00:13:35 +0700

    Can add and fix Question and Choice to database

--- quiz/views.py
+++ quiz/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 from .models import Question, Choice
-
+from django.utils import timezone
 
 def index(request):
     """ show question 5 sort by pubiher date """
@@ -27,23 +27,42 @@ def results(request, question_id):
 
 def add_question(request):
     """ add question to database """
-    question = get_object_or_404(Question, pk=question_id)
+    primary = 0
     new_text_question = "No Question add to database"
     new_text_choice1 = "No Choice add to database"
     new_text_choice2 = "No choice add to database"
+    choice1_true_false = ""
+    choice2_true_false = ""
+    
     
     if request.method == 'POST':
        new_text_question = request.POST['new_text_question']
        new_text_choice1 = request.POST['new_text_choice1']
        new_text_choice2 = request.POST['new_text_choice2']
-       choice1 = request.POST['choice1']
-       choice2 = request.POST['choice2']
+       choice1_true_false = request.POST['choice1']
+       choice2_true_false = request.POST['choice2']
+       primary = request.POST['number_question_and_choice']
+       
+       if ((Question.objects.filter(question_text__startswith= new_text_question)) == True ):
+          q = Question(question_text= new_text_question, pub_date=timezone.now())
+          q.save()
+          q = Question.objects.get(pk=primary)
+          q.choice_set.create(choice_text=new_text_choice1, votes=0)
+          q.choice_set.create(choice_text=new_text_choice2, votes=0)
+
+       elif (Question.objects.filter(question_text__startswith= new_text_question)):
+          q = Question.objects.get(pk=primary)
+          q.question_text = new_text_question
+          q.save()
+          q.choice_set.create(choice_text=new_text_choice1, votes=0)
+          q.choice_set.create(choice_text=new_text_choice2, votes=0)
+       
     show_question = { 
                    'quest_text'   : new_text_question,
                    'choice1_text' : new_text_choice1,
                    'choice2_text' : new_text_choice2,
-                   'choice1_true_false' : choice1,
-                   'choice2_true_false' : choice2
+                   'choice1_true_false' : choice1_true_false,
+                   'choice2_true_false' : choice2_true_false 
     }
     return render(request,'quiz/add_question.html',show_question )
 
